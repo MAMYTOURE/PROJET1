3f5c23db672de73a981b5f1b0ec624e59d88dc3b
MAMYTOURE <tourealmamy2002@gmail.com>
2024-06-11 13:00:35 +0000

    tache terminus

---
+++ tache1.py
@@ -0,0 +1,8 @@
+# Créer un tuple
+animaux = ("giraffe", "lion", "cat", "dog")
+# Afficher les elements du tuple
+print(animaux[0])
+print(animaux[-1])
+# Afficher les elements par tranche
+print(animaux[:2])
+print(animaux[-2:])
